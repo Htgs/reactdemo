8917348f3175fd0f81cd6844c8ac0c240093e06a
bwx9394 <298057325@qq.com>
2017-10-27 11:45:37 +0800

    [mod]使用动态组件的形式做切换

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,29 +3,27 @@ import Doc from './doc.js'
 import Guide from './guide.js'
 import Community from './community.js'
 import Blog from './blog.js'
+
+const components = {
+	doc: Doc,
+	guide: Guide,
+	community: Community,
+	blog: Blog
+}
+
+function Comp (type) {
+	let Comp = components[type]
+	return <Comp />
+}
+
 export default class Main extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
-		let comp = null
-		switch (this.props.type) {
-			case 'doc':
-			comp = <Doc />;
-			break;
-			case 'guide':
-			comp = <Guide />;
-			break;
-			case 'community':
-			comp = <Community />;
-			break;
-			case 'blog':
-			comp = <Blog />;
-			break;
-		}
 		return (
 			<div>
-				{comp}
+				{Comp(this.props.type)}
 			</div>
 		);
 	}
